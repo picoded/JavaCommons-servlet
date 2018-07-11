fc041ebd9e8d8c3c455233c66c3cb4008f76e832
Eugene Cheah <eugeneqin@gmail.com>
2018-07-11 11:19:05 +0800

    isValidEndpoint reference implementation

--- src/main/java/picoded/servlet/internal/EndpointMap.java
+++ src/main/java/picoded/servlet/internal/EndpointMap.java
@@ -108,10 +108,34 @@ public class EndpointMap<V> extends ConcurrentHashMap<String,V> {
 	 * 
 	 */
 	public boolean isValidEndpoint(String[] endpointPathArr, String[] requestPathArr) {
-		//
+		// Get some of the endpointPathArr numbers
+		int endpointPathArr_length = endpointPathArr.length;
+		int endpointPathArr_lastIndex = endpointPathArr_length - 1;
+		int requestPathArr_length = requestPathArr.length;
+
+		// Iterate each endpoint path array part
 		for(int i=0; i<endpointPathArr.length; ++i) {
 			
-			if(!endpointPathArr[i].equals(requestPathArr[i])){
+			// Get the part to compare
+			String endpointPart = endpointPathArr[i];
+			String requestPart = (requestPathArr_length <= i)? null : requestPathArr[i];
+
+			// Ending wildcard check
+			// If this triggers, its is presumed that every part segment 
+			// before was validated correctly
+			if( i == endpointPathArr_lastIndex && endpointPart.equalsIgnoreCase("*") ) {
+				return true;
+			}
+
+			// In between wildcard checks, or named paraemter checks
+			if( endpointPart.equalsIgnoreCase("*") || endpointPart.startsWith(":") ) {
+				// go to next part
+				continue;
+			}
+
+			// Finally this would be textual part to part comparision
+			// if this fails, the endpoint mapping is invalid
+			if( endpointPart.equalsIgnoreCase(requestPart) ) {
 				return false;
 			}
 		}
