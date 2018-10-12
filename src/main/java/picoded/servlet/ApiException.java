dcb8f75487637a99741b0f40ecef0a6b2decdcdb
jiaming <jiaming@uilicious.com>
2018-10-12 16:25:54 +0800

    Renamed errorCode to errorType

--- src/main/java/picoded/servlet/ApiException.java
+++ src/main/java/picoded/servlet/ApiException.java
@@ -7,48 +7,48 @@ import java.util.Map;
 
 public class ApiException extends RuntimeException {
 	private int httpStatus = 500;
-	private String errorCode = "INTERNAL_SERVER_ERROR";
+	private String errorType = "INTERNAL_SERVER_ERROR";
 	private String errorMessage = null;
-	
-	public ApiException(int httpStatus, String errorCode, String errorMessage) {
+
+	public ApiException(int httpStatus, String errorType, String errorMessage) {
 		this.httpStatus = httpStatus;
-		this.errorCode = errorCode;
+		this.errorType = errorType;
 		this.errorMessage = errorMessage;
 	}
-	
-	public ApiException(int httpStatus, String errorCode) {
+
+	public ApiException(int httpStatus, String errorType) {
 		this.httpStatus = httpStatus;
-		this.errorCode = errorCode;
+		this.errorType = errorType;
 	}
-	
-	public ApiException(String errorCode, String errorMessage) {
-		this.errorCode = errorCode;
+
+	public ApiException(String errorType, String errorMessage) {
+		this.errorType = errorType;
 		this.errorMessage = errorMessage;
 	}
-	
-	public ApiException(String errorCode) {
-		this.errorCode = errorCode;
+
+	public ApiException(String errorType) {
+		this.errorType = errorType;
 	}
-	
+
 	public ApiException(Exception e) {
 		super(e);
 	}
-	
+
 	public int getHttpStatus() {
 		return httpStatus;
 	}
-	
+
 	public String getErrorMessage() {
 		Throwable cause = this;
 		if (ExceptionUtils.getRootCause(cause) != null) {
 			cause = ExceptionUtils.getRootCause(cause);
 		}
-		
+
 		return (errorMessage == null) ? cause.getMessage() : errorMessage;
 	}
-	
+
 	public String getStackTraceString() {
-		
+
 		Throwable cause = this;
 		if (ExceptionUtils.getRootCause(cause) != null) {
 			cause = ExceptionUtils.getRootCause(cause);
@@ -56,14 +56,14 @@ public class ApiException extends RuntimeException {
 		String stackTrace = ExceptionUtils.getStackTrace(cause);
 		return stackTrace;
 	}
-	
-	public String getErrorCode() {
-		return errorCode;
+
+	public String getErrorType() {
+		return errorType;
 	}
 	
 	public Map<String, Object> getErrorMap() {
 		Map<String, Object> errorMap = new HashMap<>();
-		errorMap.put("code", getErrorCode());
+		errorMap.put("code", getErrorType());
 		errorMap.put("message", getErrorMessage());
 		errorMap.put("stack", getStackTraceString());
 		return errorMap;
